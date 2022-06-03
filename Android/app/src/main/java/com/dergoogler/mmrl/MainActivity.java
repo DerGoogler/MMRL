5324b7e3d8ba7f09efb6522efb666ddd361793f0
DerGoogler <support@dergoogler.com>
2022-06-03 20:50:05 +0200

    Remove download manager

--- Android/app/src/main/java/com/dergoogler/mmrl/MainActivity.java
+++ Android/app/src/main/java/com/dergoogler/mmrl/MainActivity.java
@@ -37,23 +37,6 @@ public class MainActivity extends AppCompatActivity {
         view.setJavascriptInterface(new BuildConfig(), "nbuildconfig");
         view.setJavascriptInterface(new OS(this), "nos");
         view.setJavascriptInterface(new SharedPreferences(this), "nsharedpreferences");
-        view.setDownloadListener((url, userAgent, contentDisposition, mimeType, contentLength) -> {
-            DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
-            request.setMimeType(mimeType);
-            //------------------------COOKIE!!------------------------
-            String cookies = CookieManager.getInstance().getCookie(url);
-            request.addRequestHeader("cookie", cookies);
-            //------------------------COOKIE!!------------------------
-            request.addRequestHeader("User-Agent", userAgent);
-            request.setDescription("Downloading file...");
-            request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimeType));
-            request.allowScanningByMediaScanner();
-            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimeType));
-            DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
-            dm.enqueue(request);
-            Toast.makeText(getApplicationContext(), "Downloading File", Toast.LENGTH_LONG).show();
-        });
     }
 
     @Override
