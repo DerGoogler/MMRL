7bdc2884584e5f242b849f59ba4bbded7a179d36
Der_Googler <support@dergoogler.com>
2022-06-06 15:09:06 +0200

    Add statics to SharedPreferences

--- Website/src/native/SharedPreferences.ts
+++ Website/src/native/SharedPreferences.ts
@@ -165,6 +165,84 @@ class SharedPreferences {
   }
 
   [name: string]: any;
+
+  // Statics
+
+  private static s: SharedPreferences = new this();
+
+  public static setString(key: string, value: string): void {
+    this.s.setString(key, value);
+  }
+
+  public static setBoolean(key: string, value: bool): void {
+    this.s.setBoolean(key, value);
+  }
+
+  public static setInt(key: string, value: int): void {
+    this.s.setInt(key, value);
+  }
+
+  /**
+   * Retrieve a String value from the preferences.
+   *
+   * @param key The name of the preference to retrieve.
+   * @param defValue Value to return if this preference does not exist.
+   *
+   * @return Returns the preference value if it exists, or defValue. Throws ClassCastException if there is a preference with this name that is not a String.
+   *
+   * @throws ClassCastException
+   */
+  public static getString(key: string, defValue: string): string | String {
+    return this.s.getString(key, defValue);
+  }
+
+  /**
+   * Retrieve a boolean value from the preferences.
+   *
+   * @param key The name of the preference to retrieve.
+   * @param defValue Value to return if this preference does not exist.
+   *
+   * @returns Returns the preference value if it exists, or defValue. Throws ClassCastException if there is a preference with this name that is not a boolean.
+   *
+   * @throws ClassCastException
+   */
+  public static getBoolean(key: string, defValue: bool): boolean | Boolean {
+    return this.s.getBoolean(key, defValue);
+  }
+
+  /**
+   * Retrieve a int value from the preferences.
+   *
+   * @param key The name of the preference to retrieve.
+   * @param defValue Value to return if this preference does not exist.
+   *
+   * @returns Returns the preference value if it exists, or defValue. Throws ClassCastException if there is a preference with this name that is not an int.
+   *
+   * @throws ClassCastException
+   */
+  public static getInt(key: string, defValue: int): number | Number {
+    return this.s.getInt(key, defValue);
+  }
+
+  /**
+   * Removes the key/value pair with the given key, if a key/value pair with the given key exists.
+   *
+   * Dispatches a storage event on Window objects holding an equivalent Storage object.
+   */
+  public static removePref(key: string): void {
+    this.s.removePref(key);
+  }
+
+  /**
+   * Removes all key/value pairs, if there are any.
+   *
+   * Dispatches a storage event on Window objects holding an equivalent Storage object.
+   */
+  public static clearPrefs(): void {
+    this.s.clearPrefs();
+  }
+
+  static [name: string]: any;
 }
 
 export default SharedPreferences;
