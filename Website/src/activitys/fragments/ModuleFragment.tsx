207a5546615cd23704c5cc6db7660256eb34178b
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2023-10-11 19:39:06 +0200

    Readd filters

--- Website/src/activitys/fragments/ModuleFragment.tsx
+++ Website/src/activitys/fragments/ModuleFragment.tsx
@@ -10,12 +10,23 @@ import { useTheme } from "@Hooks/useTheme";
 import { Page, RenderFunction } from "@Components/onsenui/Page";
 import { MissingInternet } from "@Components/MissingInternet";
 import { useNetwork } from "@Hooks/useNetwork";
-import { FilterDialog, useModuleFilter } from "@Hooks/useModulesFilter";
+import { FilterDialog, filters, useModuleFilter } from "@Hooks/useModulesFilter";
 import { Disappear } from "react-disappear";
 import { renderFunc } from "flatlist-react/lib/___subComponents/uiFunctions";
 import Paper from "@mui/material/Paper";
 import Pagination from "@mui/material/Pagination";
 import { usePagination } from "@Hooks/usePagination";
+
+import { styled, alpha } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import Menu, { MenuProps } from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import EditIcon from "@mui/icons-material/Edit";
+import Divider from "@mui/material/Divider";
+import ArchiveIcon from "@mui/icons-material/Archive";
+import FileCopyIcon from "@mui/icons-material/FileCopy";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const RenderWhenEmpty = React.memo(() => {
   const { theme } = useTheme();
@@ -46,16 +57,51 @@ export interface ModuleFragmentProps {
   renderFixed?: RenderFunction;
 }
 
+const StyledMenu = styled((props: MenuProps) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: "bottom",
+      horizontal: "right",
+    }}
+    transformOrigin={{
+      vertical: "top",
+      horizontal: "right",
+    }}
+    {...props}
+  />
+))(({ theme }) => ({
+  "& .MuiPaper-root": {
+    borderRadius: 6,
+    marginTop: theme.spacing(1),
+    minWidth: 180,
+    color: theme.palette.mode === "light" ? "rgb(55, 65, 81)" : theme.palette.grey[300],
+    boxShadow:
+      "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+    "& .MuiMenu-list": {
+      padding: "4px 0",
+    },
+    "& .MuiMenuItem-root": {
+      "& .MuiSvgIcon-root": {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      "&:active": {
+        backgroundColor: alpha(theme.palette.primary.main, theme.palette.action.selectedOpacity),
+      },
+    },
+  },
+}));
+
 const ModuleFragment = React.memo<ModuleFragmentProps>((props) => {
   const { context } = useActivity();
   const { strings } = useStrings();
   const { settings } = useSettings();
   const { theme } = useTheme();
   const { isNetworkAvailable } = useNetwork();
-  const [open, setOpen] = React.useState(false);
 
   const renderItem = React.useCallback<renderFunc<Module>>((m, k) => props.renderItem(m, k), []);
-
   const [filter, _filter, setFilter] = useModuleFilter(`${props.id}_filter`);
 
   if (!isNetworkAvailable) {
@@ -66,22 +112,68 @@ const ModuleFragment = React.memo<ModuleFragmentProps>((props) => {
     );
   }
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+  // const handleClickOpen = () => {
+  //   setOpen(true);
+  // };
 
-  const handleClose = (value: string) => {
-    setOpen(false);
-    setFilter(value);
-  };
+  // const handleClose = (value: string) => {
+  //   setOpen(false);
+  //   setFilter(value);
+  // };
 
   const [page, setPage] = React.useState(1);
 
   const modules = React.useMemo(() => props.modules, [props.modules]);
 
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const findCurrentFilter = React.useMemo(() => filters.find((f) => f.value === _filter), [_filter]);
+
   return (
     <Page renderFixed={props.renderFixed}>
       <Page.RelativeContent>
+        <Stack direction="row" justifyContent="flex-end" alignItems="center" spacing={1}>
+          <Button
+            id="demo-customized-button"
+            aria-controls={open ? "demo-customized-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            variant="contained"
+            disableElevation
+            onClick={handleClick}
+            endIcon={<KeyboardArrowDownIcon />}
+          >
+            {findCurrentFilter ? findCurrentFilter.name : "None"}
+          </Button>
+          <StyledMenu
+            id="demo-customized-menu"
+            MenuListProps={{
+              "aria-labelledby": "demo-customized-button",
+            }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={() => {
+              setAnchorEl(null);
+            }}
+          >
+            {filters.map((fil) => (
+              <MenuItem
+                onClick={() => {
+                  setFilter(fil.value);
+                  setAnchorEl(null);
+                }}
+                disableRipple
+              >
+                <fil.icon />
+                {fil.name}
+              </MenuItem>
+            ))}
+          </StyledMenu>
+        </Stack>
         {/* <Paper
           component={Stack}
           elevation={0}
@@ -127,8 +219,6 @@ const ModuleFragment = React.memo<ModuleFragmentProps>((props) => {
           />
         </Box>
       </Page.RelativeContent>
-
-      <FilterDialog selectedValue={_filter} open={open} onClose={handleClose} />
     </Page>
   );
 });
