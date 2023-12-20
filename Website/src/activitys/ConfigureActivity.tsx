f2bab5886f06e1b635a802e6ea1bcd8e14ef0be8
LittleDipshit <147397299+LittleDipshit@users.noreply.github.com>
2023-12-20 10:32:57 +0100

    Disable default page in Module Configure

--- Website/src/activitys/ConfigureActivity.tsx
+++ Website/src/activitys/ConfigureActivity.tsx
@@ -1,5 +1,3 @@
-import { Page } from "@Components/onsenui/Page";
-import { Toolbar } from "@Components/onsenui/Toolbar";
 import { useActivity } from "@Hooks/useActivity";
 import React from "react";
 import { SuFile } from "@Native/SuFile";
@@ -31,21 +29,8 @@ const ConfigureActivity = () => {
     }
   }, []);
 
-  const renderToolbar = () => {
-    return (
-      <Toolbar modifier="noshadow">
-        <Toolbar.Left>
-          <Toolbar.BackButton onClick={context.popPage} />
-        </Toolbar.Left>
-        <Toolbar.Center>Configure {extra.modulename}</Toolbar.Center>
-      </Toolbar>
-    );
-  };
-
   return (
-    <Page renderToolbar={renderToolbar}>
-      <PreviewErrorBoundary modid={extra.moduleid} children={config} renderElement={ConfigureView} />
-    </Page>
+    <PreviewErrorBoundary modid={extra.moduleid} children={config} renderElement={ConfigureView} />
   );
 };
 
