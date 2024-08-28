136bdc7885b7004ab98818512928b2958de07f53
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2024-08-28 17:15:38 +0200

    add missing japanese declaration

--- src/locales/declaration.ts
+++ src/locales/declaration.ts
@@ -1,17 +1,18 @@
+import React from "react";
 import { AvailableLangs, useStrings } from "@Hooks/useStrings";
 import { de } from "./de";
 import { en } from "./en";
 import { zh } from "./zh";
 import { pt_BR } from "./pt-BR";
-import { az_AZ } from "./az-AZ"
-import React from "react";
+import { az_AZ } from "./az-AZ";
+import { ja } from "./ja";
 
 /**
  * English is the default language
  */
 export type StringDeclaration = keyof typeof en;
 
-export type AvailableStrs = "en" | "de" | "zh" | "pt-BR" | "az-AZ";
+export type AvailableStrs = "en" | "de" | "zh" | "pt-BR" | "az-AZ" | "ja";
 export type Strs = {
   [code in AvailableStrs]: Partial<Record<StringDeclaration, str>>;
 };
@@ -22,6 +23,7 @@ export const strs: Strs = {
   zh: zh,
   "pt-BR": pt_BR,
   "az-AZ": az_AZ,
+  ja: ja,
 };
 
 export const useLanguageMap = (): arr<AvailableLangs> => {
