393f043d9a5b88472ab4ffbd6e1f207a27c43645
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2024-11-30 20:29:38 +0100

    add texts to module search

--- app/src/main/kotlin/com/dergoogler/mmrl/ui/screens/repository/view/FilteredSearchScreen.kt
+++ app/src/main/kotlin/com/dergoogler/mmrl/ui/screens/repository/view/FilteredSearchScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -14,18 +15,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.pluralStringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.dergoogler.mmrl.R
+import com.dergoogler.mmrl.ui.component.HtmlText
 import com.dergoogler.mmrl.ui.component.Loading
 import com.dergoogler.mmrl.ui.component.PageIndicator
+import com.dergoogler.mmrl.ui.providable.LocalUserPreferences
 import com.dergoogler.mmrl.ui.screens.repository.ModulesList
 import com.dergoogler.mmrl.ui.utils.none
 import com.dergoogler.mmrl.viewmodel.RepositoryViewModel
-import timber.log.Timber
+import ext.dergoogler.mmrl.ext.takeTrue
 
 @Composable
 fun FilteredSearchScreen(
@@ -37,6 +43,7 @@ fun FilteredSearchScreen(
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
     val list by viewModel.online.collectAsStateWithLifecycle()
     val listState = rememberLazyListState()
+    val userPrefs = LocalUserPreferences.current
 
     LaunchedEffect(list, value) {
         if (!value.isNullOrBlank() && !type.isNullOrBlank()) {
@@ -77,6 +84,24 @@ fun FilteredSearchScreen(
             }
 
             ModulesList(
+                before = {
+                    HtmlText(
+                        modifier = Modifier.padding(vertical = 8.dp),
+                        text = pluralStringResource(
+                            id = R.plurals.search_results,
+                            count = list.size,
+                            MaterialTheme.colorScheme.surfaceTint.toArgb(),
+                            list.size
+                        )
+                    )
+
+                    userPrefs.developerMode.takeTrue {
+                        Text(
+                            text = "$type:$value",
+                            style = MaterialTheme.typography.bodySmall.copy(color = MaterialTheme.colorScheme.outline),
+                        )
+                    }
+                },
                 list = list,
                 state = listState,
                 navController = navController
