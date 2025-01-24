ac08c551b1ecec07fda699e12ecc3986da2d8bfb
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2025-01-25 00:29:39 +0100

    add pull to refresh to installed modules

---
+++ app/src/main/kotlin/com/dergoogler/mmrl/ui/component/PullToRefresh.kt
@@ -0,0 +1,40 @@
+package com.dergoogler.mmrl.ui.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults.Indicator
+import androidx.compose.material3.pulltorefresh.PullToRefreshState
+import androidx.compose.material3.pulltorefresh.pullToRefresh
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun PullToRefreshBox(
+    isRefreshing: Boolean,
+    onRefresh: () -> Unit,
+    modifier: Modifier = Modifier,
+    state: PullToRefreshState = rememberPullToRefreshState(),
+    contentAlignment: Alignment = Alignment.TopStart,
+    indicator: @Composable BoxScope.() -> Unit = {
+        Indicator(
+            modifier = Modifier.align(Alignment.TopCenter),
+            isRefreshing = isRefreshing,
+            state = state
+        )
+    },
+    content: @Composable BoxScope.() -> Unit,
+) {
+    Box(
+        modifier = modifier.pullToRefresh(
+            state = state,
+            isRefreshing = isRefreshing,
+            onRefresh = onRefresh
+        ),
+        contentAlignment = contentAlignment
+    ) {
+        content()
+        indicator()
+    }
+}
